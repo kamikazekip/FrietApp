a3505ec34113581b24d52614952b6d2dedf9b7b5
kamikazekip <user@Users-Mac.local>
2015-03-11 04:48:20 -0700

    Segues toegevoegd
    Userstart

--- FrietApp/ViewController.swift
+++ FrietApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextfield: UITextField!
+    @IBOutlet weak var warningLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func register(sender: UIButton) {
+        
+    }
+    
+    func registerAttemptDone(outcome: Bool){
+        if(outcome){
+            self.performSegueWithIdentifier("register", sender: nil)
+        } else {
+            warningLabel.hidden = false
+            warningLabel.text = "Deze gebruikersnaam bestaat al!"
+            usernameTextfield.text = ""
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier == "register"){
+            var overviewVC: OverviewController = segue.destinationViewController as OverviewController
+            overviewVC.receivedUsername = usernameTextfield.text
+        }
+    }
 }
 
