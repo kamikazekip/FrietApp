0f7dda3cf526b570e51a62e0da5e3a59366fc78b
KamikazeChicken <ebrandsm@pc131120.local>
2015-04-10 17:46:46 +0200

    Laatste

--- FrietApp/MakeOrderController.swift
+++ FrietApp/MakeOrderController.swift
@@ -9,13 +9,16 @@
 import UIKit
 import CoreLocation
 
-class MakeOrderController: UIViewController, CLLocationManagerDelegate {
+class MakeOrderController: UIViewController, NSURLConnectionDelegate, CLLocationManagerDelegate, UITableViewDelegate, UITableViewDataSource {
 
     var oldController: OrderListController!
     let locationManager = CLLocationManager()
     
-    var latitude: Double!
-    var longtitude: Double!
+    var defaults = NSUserDefaults.standardUserDefaults()
+    
+    var lastStatusCode = 1
+    var data: NSMutableData = NSMutableData()
+    var snackbars: [Snackbar]! = []
     
     // viewDidLoad
     override func viewDidLoad() {
@@ -27,15 +30,70 @@ class MakeOrderController: UIViewController, CLLocationManagerDelegate {
     }
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        println("HALLO")
         var locValue : CLLocationCoordinate2D = manager.location.coordinate
-        println(locValue)
         locationManager.stopUpdatingLocation()
-        getSnackbarsWithLocation(locValue.latitude, longtitude: locValue.longitude)
+        getSnackbars(locValue.latitude, longtitude: locValue.longitude)
     }
     
-    func getSnackbarsWithLocation(latitude: Double, longtitude: Double){
+    func getSnackbars(latitude: Double, longtitude: Double){
         println(latitude)
         println(longtitude)
+        let base64LoginString = defaults.valueForKey("authHeader") as! String
+        println(base64LoginString)
+        // create the request
+        var urlString = "https://desolate-bayou-9128.herokuapp.com/snackbars/?lat=\(latitude)&long=\(longtitude)"
+
+        let url = NSURL(string: urlString)
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "GET"
+        request.setValue(base64LoginString, forHTTPHeaderField: "Authorization")
+        
+        let urlConnection = NSURLConnection(request: request, delegate: self)
+    }
+    
+    //NSURLConnection delegate method
+    func connection(connection: NSURLConnection, didFailWithError error: NSError) {
+        println("Failed with error:\(error.localizedDescription)")
+    }
+    
+    //NSURLConnection delegate method
+    func connection(didReceiveResponse: NSURLConnection!, didReceiveResponse response: NSHTTPURLResponse!) {
+        //New request so we need to clear the data object
+        self.lastStatusCode = response.statusCode;
+        self.data = NSMutableData()
+    }
+    
+    //NSURLConnection delegate method
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
+        //Append incoming data
+        self.data.appendData(data)
+    }
+    
+    //NSURLConnection delegate method
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        if(self.lastStatusCode == 200){
+            let json: AnyObject! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil)
+            println(json)
+            let receivedSnackbars = json as! [[String :AnyObject]]!
+            println(receivedSnackbars)
+            for snackbar in receivedSnackbars {
+                let name = snackbar["snackbar"]! as! String
+                let url  = snackbar["url"]! as! String
+                snackbars.insert(Snackbar(name: name, url: url), atIndex: 0)
+            }
+            println(snackbars)
+        } else {
+            println("Something went wrong, statusCode wasn't 200")
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return snackbars.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("snackbarCell") as! OrderCell
+        
+        return cell
     }
 }
---
+++ FrietApp/Snackbar.swift
@@ -0,0 +1,19 @@
+//
+//  Snackbar.swift
+//  FrietApp
+//
+//  Created by Erik Brandsma on 10/04/15.
+//  Copyright (c) 2015 Erik Brandsma. All rights reserved.
+//
+
+import Foundation
+
+class Snackbar {
+    var name: String!
+    var url: String!
+    
+    init(name: String, url: String){
+        self.name = name
+        self.url = url
+    }
+}
